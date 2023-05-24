0c829ad44bbe0005d0e2382016eafd813388f33d
Luis Alejandro Torres Escalera <88306161+late1x@users.noreply.github.com>
2023-05-24 08:30:04 -0600

    update database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize('defaultdb', 'doadmin', 'AVNS_mKTTNQRoLWI3LS6il0N', {
+    host: "db-mysql-tor1-37393-do-user-6926676-0.b.db.ondigitalocean.com",
+    port: '25060',
     dialect: "mysql"
 });
 
